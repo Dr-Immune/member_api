39c9350b0fd670adf191f60eba8a29541cd955d2
Hosein Rezapoor <drimmune@localhost.localdomain>
2020-09-12 15:47:38 +0100

    set all the route for the app

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/members', methods=['GET'])
+@app.route('/member', methods=['GET'])
 def members():
-    return 'this is working'
+    return None
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def member(member_id):
+    return None
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    return None
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def edit_member(member_id):
+    return None
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    return None
 
 if __name__ == "__main__":
     app.run(debug=True)

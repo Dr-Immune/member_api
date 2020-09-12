c5d5fdf53158f1e27d58283fee66a1a39229d7cb
Hosein Rezapoor <drimmune@localhost.localdomain>
2020-09-13 01:50:16 +0430

    DB config and add_member method

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'member_api'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 @app.route('/member', methods=['GET'])
 def members():
@@ -12,7 +19,24 @@ def member(member_id):
 
 @app.route('/member', methods=['POST'])
 def add_member():
-    return None
+    cur = mysql.connection.cursor()
+    data = request.get_json()
+    new_member = data['new_member']
+    name = new_member['name']
+    email = new_member['email']
+    level = new_member['level']
+
+    cur.execute('INSERT INTO members(name, email, level) VALUES(%s, %s, %s)', [name, email, level])
+    mysql.connection.commit()
+
+    cur.execute('SELECT * FROM members WHERE name = %s ', [name])
+    result = cur.fetchone()
+
+    if result == None:
+        return Response('error', status=400)
+        # TODO: RETURN PROPER MESSAGE
+    else:
+        return jsonify(result), 201
 
 @app.route('/member/<int:member_id>', methods=['PUT'])
 def edit_member(member_id):

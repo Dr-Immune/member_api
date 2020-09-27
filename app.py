9fd5e01ed09a2b2fb05f10b1d73d6583c0fbc538
Hosein Rezapoor <drimmune@localhost.localdomain>
2020-09-27 21:15:41 +0330

    use sqlalchemy for connect to db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, Response
+from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
@@ -9,7 +10,27 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'member_api'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/member_api'
 mysql = MySQL(app)
+db = SQLAlchemy(app)
+
+class Members(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    level = db.Column(db.String(10), nullable=False)    
+
+    def __repr__(self):
+        return '<user %r>' % self.name
+
+
+class Users(db.Model):
+    username = db.Column(db.String(20), primary_key=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<user %r>' % self.username
+
 
 def authentication(f):
     @wraps(f)
@@ -18,12 +39,9 @@ def authentication(f):
         if auth:
             received_username = auth.username
             received_password = auth.password
-            cur = mysql.connection.cursor()
-            cur.execute('SELECT password FROM users WHERE username = %s', [received_username])
-            result = cur.fetchone()
-            cur.close()
 
-            if check_password_hash(result['password'], received_password):
+            user = Users.query.filter_by(username = received_username).first()
+            if check_password_hash(user.password, received_password):
                 return f(*args, **kwargs)
             return jsonify({"status": "authentication failed"}), 401
         return jsonify({"status": "no authentication parameters"}), 400
@@ -33,48 +51,52 @@ def authentication(f):
 @app.route('/member', methods=['GET'])
 @authentication
 def members():
-    cur = mysql.connect.cursor()
-    cur.execute('SELECT * FROM members')
-    result = cur.fetchall()
-    cur.close()
+    result = db.session.query(Members.id, Members.name, Members.email, Members.level)
 
-    return jsonify(result)
+    json_result = []
+    for item in result:
+        json_result.append(item._asdict())
+
+    return jsonify(json_result)
 
 @app.route('/member/<int:member_id>', methods=['GET'])
 @authentication
 def member(member_id):
-    cur = mysql.connect.cursor()
-    cur.execute('SELECT * FROM members WHERE id = %s', [member_id])
-    result = cur.fetchone()
-    cur.close()
-
-    if result == None:
-        print('str')
+    result_count = Members.query.filter(Members.id == member_id).count()
+    if result_count == 0:
         return Response('Member not found', status=404)
-    return jsonify(result)
+
+    result = db.session.query(Members.id, Members.name, Members.email, Members.level).filter(Members.id == member_id)
+    json_result = []
+    for item in result:
+        json_result.append(item._asdict())
+
+    return jsonify(json_result)
 
 @app.route('/member', methods=['POST'])
 @authentication
-def add_member():
-    cur = mysql.connection.cursor()
+def add_member(): 
     data = request.get_json()
     new_member = data['new_member']
     name = new_member['name']
     email = new_member['email']
     level = new_member['level']
 
-    cur.execute('INSERT INTO members(name, email, level) VALUES(%s, %s, %s)', [name, email, level])
-    mysql.connection.commit()
+    new_member_query = Members(name=name, email= email, level=level)
+    db.session.add(new_member_query)
+    db.session.commit()
 
-    cur.execute('SELECT * FROM members WHERE name = %s ', [name])
-    result = cur.fetchone()
-    cur.close()
-
-    if result == None:
+    result_count = Members.query.filter_by(name = name).count()
+    if result_count == 0 :
         return Response('error', status=400)
-        # TODO: RETURN PROPER MESSAGE
-    else:
-        return jsonify(result), 201
+
+
+    result = db.session.query(Members.id, Members.name, Members.email, Members.level).filter(Members.name == name)
+    json_result = []
+    for item in result:
+        json_result.append(item._asdict())
+
+    return jsonify(json_result), 201
 
 @app.route('/member/<int:member_id>', methods=['PUT'])
 @authentication
@@ -84,33 +106,36 @@ def edit_member(member_id):
     email = new_member_data['email']
     level = new_member_data['level']
 
-    cur = mysql.connect.cursor()
-    cur.execute('SELECT name FROM members WHERE id = %s', [member_id])
-    check_result = cur.fetchone()
-    if check_result == None:
+    result_count = Members.query.filter(Members.id == member_id).count()
+    if result_count == 0:
         return Response('User not found', status=404)
-        
-    cur.execute('UPDATE members SET name = %s, email = %s, level = %s WHERE id = %s', [name, email, level, member_id])
-    cur.connection.commit()
 
-    cur.execute('SELECT id, name, email, level FROM members WHERE id = %s', [member_id])
-    result = cur.fetchone()
-    cur.close()
+    user = Members.query.filter(Members.id == member_id).first()
+    user.name = name
+    user.email = email
+    user.level = level
+    db.session.commit()
+
+    result = {"id": user.id, "name": user.name, "email": user.email, "level": user.level}
 
     return jsonify(result)
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 @authentication
 def delete_member(member_id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT name FROM members WHERE id = %s', [member_id])
-    check_result = cur.fetchone()
-    if check_result == None:
+    user_exist = Members.query.filter_by(id = member_id).count()
+    if user_exist == 0:
         return Response('User not found', status=404)
-    cur.execute('DELETE FROM members WHERE id = %s', [member_id])
-    cur.connection.commit()
-    cur.close()
-    return Response('Member deleted successfuly', status=200)
+    
+    user = Members.query.filter_by(id = member_id).first()
+    db.session.delete(user)
+    db.session.commit()
+
+    user_exist = Members.query.filter_by(id = member_id).count()
+    if user_exist == 0:
+        return Response('Member deletsed successfuly', status=200)
+    return Response('Member not deleted')
+
 
 @app.route('/user', methods=['POST'])
 def add_user():
@@ -119,21 +144,16 @@ def add_user():
     password = new_user['password']
     hashed_password = generate_password_hash(password, method='sha256')
 
-    cur = mysql.connect.cursor()
-    cur.execute('SELECT username FROM users WHERE username = %s', [username])
-    result = cur.fetchone()
-
-    if result != None:
+    user_exist = Users.query.filter_by(username = username).count()
+    if user_exist == 1:
         return Response('User exist, try another username', status=400)
 
-    cur.execute('INSERT INTO users(username, password) VALUES(%s, %s)', [username, hashed_password])
-    cur.connection.commit()
+    new_user = Users(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
 
-    cur.execute('SELECT username FROM users WHERE username = %s', [username])
-    result = cur.fetchone()
-    cur.close()
-
-    if result == None:
+    user_exist = Users.query.filter_by(username = username).count()
+    if user_exist == 0:
         return Response('User not created', status=500)
 
     return jsonify({"status": "User created", "User": {"username": username, "password": password}})
@@ -144,12 +164,8 @@ def check_user():
     username = userinfo['username']
     password = userinfo['password']
 
-    cur = mysql.connect.cursor()
-    cur.execute('SELECT username, password FROM users WHERE username = %s', [username])
-    result = cur.fetchone()
-    cur.close()
-    
-    check_result = check_password_hash(result['password'], password)
+    user = Users.query.filter_by(username = username).first()
+    check_result = check_password_hash(user.password, password)
 
     return jsonify(check_result)
 
